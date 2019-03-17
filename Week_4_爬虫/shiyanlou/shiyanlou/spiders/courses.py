b8835e38ff847fec2199de9d06560ceac420a3be
Sang-TianYue <43460542+Sang-TianYue@users.noreply.github.com>
2019-03-17 14:20:59 +0800

    Update courses.py

--- Week_4_爬虫/shiyanlou/shiyanlou/spiders/courses.py
+++ Week_4_爬虫/shiyanlou/shiyanlou/spiders/courses.py
@@ -6,14 +6,14 @@ class CoursesSpider(scrapy.Spider):
     name = 'courses'
     # allowed_domains = ['shiyanlou.com']
 
-    @property
+    @property 
     def start_urls(self):
         url_tmpl = 'https://www.shiyanlou.com/courses/?category=all&course_type=all&fee=all&tab=all&page={}'
         return (url_tmpl.format(i) for i in range(1, 24))
 
     def parse(self, response):
         for course in response.css("div.course-body"):
-            # a CourseItem class instance
+            # 将parse方法的返回值使用item表示，包装为CourseItem
             item = CourseItem({
                 'name':course.css('div.course-name::text').extract_first(),
                 'description':course.css('div.course-desc::text').extract_first(),
